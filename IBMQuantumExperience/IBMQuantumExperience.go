d19a0ea993ba52edb53fad0e7f2ed430542ec9e1
Liam Conrad <liam@storskegg.org>
2018-07-28 11:04:01 -0500

    empty test, define methods

---
+++ IBMQuantumExperience/IBMQuantumExperience.go
@@ -0,0 +1,23 @@
+package IBMQuantumExperience
+
+func getJobURL() {}
+
+func getBackendStatsURL() {}
+
+func getBackendURL() {}
+
+func cleanQObjectResult() {}
+
+type credentials struct {}
+
+type request struct {}
+
+type IBMQuantumExperience struct {}
+
+type ApiError error
+
+type BadBackendError ApiError
+
+type CredentialsError ApiError
+
+type RegisterSizeError ApiError
